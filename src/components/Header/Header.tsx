df1c17b5cb55fffd00106415a5e72eca58470fb5
RAJAPANDIAN P <rajapandian2003rp5555@gmail.com>
2024-05-16 11:48:59 +0530

    code correction

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { Container } from './styles'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { NavHashLink, HashLink } from 'react-router-hash-link'
 import { useState } from 'react'
-import Resume from '../../assets/Kabitha - Resume.pdf'
+import Resume from '../../assets/Kabitha Resume.pdf'
 export function Header() {
   const [isActive, setActive] = useState(false)
   function toggleTheme() {
